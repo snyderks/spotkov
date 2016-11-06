de4946fe6039e21b7d9827c0cd53c2abd25b8a82
Kristian Snyder <knsnyder96@gmail.com>
2016-11-06 17:31:55 -0500

    LastFM page read changes Now checks to see if there is a song currently playing and doesn't include it Correctly reverses the songs now - before the first song was the most recent, now it should be the last one

--- lastFm/lastFm.go
+++ lastFm/lastFm.go
@@ -33,10 +33,11 @@ type metadata struct {
 }
 
 type track struct {
-	Artist    artist    `json:"artist"`
-	Title     string    `json:"name"`
-	Album     album     `json:"album"`
-	Timestamp trackDate `json:"date"`
+	Artist     artist                 `json:"artist"`
+	Title      string                 `json:"name"`
+	Album      album                  `json:"album"`
+	Timestamp  trackDate              `json:"date"`
+	Attributes map[string]interface{} `json:"@attr"`
 }
 
 type trackDate struct {
@@ -88,7 +89,25 @@ func ReadLastFMSongs(user_id string) []Song {
 
 	songs := SongsPage{}
 	err = json.Unmarshal(songsJSON, &songs)
-
+	// We don't want the currently playing track there. This checks for that.
+	containsNowPlaying := false
+	if len(songs.RecentTracks.Tracks) > 0 {
+		if songs.RecentTracks.Tracks[0].Attributes != nil &&
+			songs.RecentTracks.Tracks[0].Attributes["nowplaying"].(string) == "true" {
+			containsNowPlaying = true
+		}
+	}
+	if containsNowPlaying {
+		songs.RecentTracks.Tracks = songs.RecentTracks.Tracks[1:]
+	}
+	topIndex := len(songs.RecentTracks.Tracks) - 1
+	for i := topIndex; i >= 0; i-- {
+		if topIndex-i != i {
+			temp := songs.RecentTracks.Tracks[i]
+			songs.RecentTracks.Tracks[i] = songs.RecentTracks.Tracks[topIndex-i]
+			songs.RecentTracks.Tracks[topIndex-i] = temp
+		}
+	}
 	pageSongs := make([]Song, 0, 50)
 
 	for _, track := range songs.RecentTracks.Tracks {
@@ -113,6 +132,15 @@ func ReadLastFMSongs(user_id string) []Song {
 
 	pagesWg.Wait()
 
+	topIndex = len(songPages) - 1
+	for i := topIndex; i >= 0; i-- {
+		if topIndex-i != i {
+			temp := songPages[i]
+			songPages[i] = songPages[topIndex-i]
+			songPages[topIndex-i] = temp
+		}
+	}
+
 	titlesConcat := make([]Song, 0)
 
 	for i := 0; i < max_page; i++ {
@@ -132,14 +160,35 @@ func getLastFMPagesAsync(url string, page int, max_page int, allTitles [][]Song)
 		songsJSON, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
 		err = json.Unmarshal(songsJSON, &songs)
-
 		if err != nil {
 			log.Fatal("couldn't read the body of the last.fm response")
 		}
 	} else {
 		fmt.Println(err)
 	}
-	tracksRaw := songs.RecentTracks.Tracks
+	var tracksRaw []track
+	// Eliminate currently playing track if returned.
+	containsNowPlaying := false
+	if len(songs.RecentTracks.Tracks) > 0 {
+		if songs.RecentTracks.Tracks[0].Attributes != nil &&
+			songs.RecentTracks.Tracks[0].Attributes["nowplaying"].(string) == "true" {
+			containsNowPlaying = true
+		}
+	}
+	if containsNowPlaying {
+		tracksRaw = songs.RecentTracks.Tracks[1:]
+	} else {
+		tracksRaw = songs.RecentTracks.Tracks
+	}
+	// Reverse the array so that the suffixes are built in the right order.
+	topIndex := len(tracksRaw) - 1
+	for i := topIndex; i >= 0; i-- {
+		if topIndex-i != i {
+			temp := tracksRaw[i]
+			tracksRaw[i] = tracksRaw[topIndex-i]
+			tracksRaw[topIndex-i] = temp
+		}
+	}
 	titles := make([]Song, 0)
 	for _, track := range tracksRaw {
 		utime, err := strconv.ParseInt(track.Timestamp.UnixTime, 10, 64)
