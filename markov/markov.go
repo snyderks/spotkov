145cd40e00f1f36c912bd9269deadde00c34abb7
Kristian Snyder <knsnyder96@gmail.com>
2016-11-26 13:34:44 -0500

    Generation optimization Fixed issue where a repeat of four or more at the beginning would trigger the removal of duplicate sequence Removed ability to add repeats entirely (not worth the trouble)

--- markov/markov.go
+++ markov/markov.go
@@ -3,13 +3,12 @@
 package markov
 
 import (
-	"sort"
-	"math/rand"
-	"time"
-	"fmt"
-	"math"
 	"errors"
+	"fmt"
+	"math/rand"
+	"sort"
 	"strings"
+	"time"
 	"unicode"
 
 	"github.com/snyderks/spotkov/lastFm"
@@ -24,18 +23,12 @@ type Suffixes struct {
 type Suffix struct {
 	Name      string
 	Artist    string // for more accurate lookup in Spotify
-	Frequency int // number of times the suffix happens
+	Frequency int    // number of times the suffix happens
 }
 
 type CDF [][2]int
 
-const repeatDiscount = 0.5
-/* the percentage of the Chance to discount
- * the suffix to by if it's a repeat of the prefix
- * AFTER TAKING THE NATURAL LOG OF IT
- */
-
- const maxDeletionAttempts = 200
+const maxDeletionAttempts = 200
 
 func BuildChain(songs []lastFm.Song) map[string]Suffixes {
 	// A prefix length of 1 is used (for now, it makes it super easy)
@@ -45,22 +38,24 @@ func BuildChain(songs []lastFm.Song) map[string]Suffixes {
 			suffixes, exists := chain[song.Title] // try and get the suffixes
 			if exists {
 				nextSong := songs[i+1]
-				timeSplit := song.Timestamp.Sub(nextSong.Timestamp)
-				if timeSplit < time.Hour {
-					var found bool = false
-					for i, suffix := range suffixes.Suffixes {
-						if suffix.Name == nextSong.Title {
-							suffixes.Suffixes[i].Frequency += 1
-							found = true
-							break
+				if nextSong.Title != song.Title || nextSong.Artist != song.Artist { // don't want to add duplicates
+					timeSplit := song.Timestamp.Sub(nextSong.Timestamp)
+					if timeSplit < time.Hour {
+						var found bool = false
+						for i, suffix := range suffixes.Suffixes {
+							if suffix.Name == nextSong.Title {
+								suffixes.Suffixes[i].Frequency += 1
+								found = true
+								break
+							}
 						}
+						if found == false {
+							suffixes.Suffixes = append(suffixes.Suffixes,
+								Suffix{Name: nextSong.Title, Artist: nextSong.Artist, Frequency: 1})
+						}
+						suffixes.Total += 1
+						chain[song.Title] = suffixes
 					}
-					if found == false {
-						suffixes.Suffixes = append(suffixes.Suffixes, 
-							Suffix{Name: nextSong.Title, Artist: nextSong.Artist, Frequency: 1})
-					}
-					suffixes.Total += 1
-					chain[song.Title] = suffixes
 				}
 			} else {
 				suffix := Suffix{Name: songs[i+1].Title, Artist: songs[i+1].Artist, Frequency: 1}
@@ -72,7 +67,7 @@ func BuildChain(songs []lastFm.Song) map[string]Suffixes {
 	return chain
 }
 
-func GenerateSongList(length int, startingSong lastFm.Song, chain map[string]Suffixes) ([]lastFm.Song, error){
+func GenerateSongList(length int, startingSong lastFm.Song, chain map[string]Suffixes) ([]lastFm.Song, error) {
 	repeats := 0 // count of the number of repeats in a row
 	deletionAttempts := 0
 	clearAttempts := false
@@ -81,9 +76,9 @@ func GenerateSongList(length int, startingSong lastFm.Song, chain map[string]Suf
 	list := make([]lastFm.Song, 0, length)
 	list = append(list, startingSong)
 	// Basic length loop
-	for i := 0; i < length; i++ {
-		foundSuffix = false;
-		/* 
+	for i := 0; i < length-1; i++ {
+		foundSuffix = false
+		/*
 		 * Start at the end of the list and use that as the prefix.
 		 * Try it and if it doesn't work, keep going back to the start.
 		 * If we reach the start of the list and it still can't find a suffix,
@@ -93,7 +88,7 @@ func GenerateSongList(length int, startingSong lastFm.Song, chain map[string]Suf
 			song, err := selectSuffix(chain, list[j].Title, &repeats)
 			if err == nil {
 				list = append(list, song)
-				foundSuffix = true;
+				foundSuffix = true
 			} else {
 				return list, err
 			}
@@ -109,11 +104,11 @@ func GenerateSongList(length int, startingSong lastFm.Song, chain map[string]Suf
 			if deleted {
 				// override the loop index because any deleted songs can't
 				// be included in the length. Have to go back 2 due to the increment.
-				i = len(list) - 2 
+				i = len(list) - 2
 				deletionAttempts++
 				clearAttempts = false
 			} else if deleted == false {
-				/* 
+				/*
 				 * Sometimes there are cycles that the generation gets in where it will
 				 * add duplicates, go to another sequence that also contains duplicates,
 				 * and repeat that. Requiring two successful additions should help. May
@@ -134,10 +129,6 @@ func GenerateSongList(length int, startingSong lastFm.Song, chain map[string]Suf
 	return list, genError
 }
 
-func adjustRepeatFrequency(baseFreq int, repeats int) int {
-	return int(math.Log(float64(baseFreq)) * (repeatDiscount / float64(repeats)))
-}
-
 func findDuplicateSequences(originalList *[]lastFm.Song) (list []lastFm.Song, deleting bool, err error) {
 	list = *originalList
 	err = nil
@@ -153,7 +144,7 @@ func findDuplicateSequences(originalList *[]lastFm.Song) (list []lastFm.Song, de
 
 		var j int
 		if len(indicesToDelete) > 0 {
-			j = indicesToDelete[len(indicesToDelete) - 1] + 1 
+			j = indicesToDelete[len(indicesToDelete)-1] + 1
 			// The loop goes backwards. No need to start from the end.
 			// Checking the next one first is plenty.
 		} else {
@@ -175,7 +166,7 @@ func findDuplicateSequences(originalList *[]lastFm.Song) (list []lastFm.Song, de
 		// This check is to determine if it's found multiple duplicates in a row
 		// and to make sure either the last one is at the end (can't continue) or
 		// if it hit the end of the sequence.
-		if len(indicesToDelete) > 1 && (found == false || indicesToDelete[len(indicesToDelete) - 1] == len(list) - 1) {
+		if len(indicesToDelete) > 1 && (found == false || indicesToDelete[len(indicesToDelete)-1] == len(list)-1) {
 			deleting = true
 			break
 		}
@@ -184,13 +175,13 @@ func findDuplicateSequences(originalList *[]lastFm.Song) (list []lastFm.Song, de
 		deleted := 0
 		for _, index := range indicesToDelete {
 			index -= deleted
-			if index < len(list) - 1 {
+			if index < len(list)-1 {
 				list = append(list[:index], list[index+1:]...)
 			} else {
 				list = list[:index]
 			}
 			deleted++
-			
+
 		}
 	}
 	return list, deleting, err
@@ -198,12 +189,12 @@ func findDuplicateSequences(originalList *[]lastFm.Song) (list []lastFm.Song, de
 }
 
 func selectSuffix(chain map[string]Suffixes, prefix string, repeats *int) (lastFm.Song, error) {
-	prefix = strings.Map(func (r rune) rune {
-			if unicode.IsPunct(r) == true {
-				return -1
-			}
-			return r
-		}, strings.ToLower(prefix))
+	prefix = strings.Map(func(r rune) rune {
+		if unicode.IsPunct(r) == true {
+			return -1
+		}
+		return r
+	}, strings.ToLower(prefix))
 	exists := false
 	for key := range chain {
 		fmtPrefix := utils.LowerAndStripPunct(prefix)
@@ -211,59 +202,45 @@ func selectSuffix(chain map[string]Suffixes, prefix string, repeats *int) (lastF
 		if fmtKey == fmtPrefix || strings.HasPrefix(fmtKey, fmtPrefix) {
 			exists = true
 			// It might be slightly different in the chain. This will allow it to continue if it is.
-			prefix = key 
+			prefix = key
 			break
-		} 
+		}
 	}
 	song := lastFm.Song{}
-		if exists == true {
-			if len(chain[prefix].Suffixes) > 1 {
-				suffixes := chain[prefix].Suffixes
-				cdf := make(CDF, 0, len(suffixes)) // cumulative distribution array with index 0 as the value, 1 as the Suffix index
-				for j, suffix := range suffixes {
-					var freq int
-					suffixName := strings.Map(func (r rune) rune {
-						if unicode.IsPunct(r) == true {
-							return -1
-						}
-						return r
-					}, strings.ToLower(suffix.Name))
-					// Case insensitive comparison and prefix matching (substring is a bad idea)
-					// because many songs have similar names
-					if suffixName == prefix || strings.HasPrefix(suffixName, prefix) {
-						*repeats = *repeats + 1
-						freq = adjustRepeatFrequency(suffix.Frequency, *repeats)
-					} else {
-						freq = suffix.Frequency
-						*repeats = 0 // wasn't a repeat, reset
-					}
-					if (freq > 0) {
-						cdf = append(cdf, [2]int {freq, j})
-					}
-				}
+	if exists == true {
+		if len(chain[prefix].Suffixes) > 1 {
+			suffixes := chain[prefix].Suffixes
+			cdf := make(CDF, 0, len(suffixes)) // cumulative distribution array with index 0 as the value, 1 as the Suffix index
+			for j, suffix := range suffixes {
+				freq := suffix.Frequency
 
-				sort.Sort(cdf) // making the CDF is much easier with sorting first.
-
-				// Creating the cdf here
-				for j := 1; j < len(cdf); j++ {
-					cdf[j][0] = cdf[j-1][0]
+				if freq > 0 {
+					cdf = append(cdf, [2]int{freq, j})
 				}
-				// Now to do the search
-				suffix := suffixes[searchCDF(cdf)]
-				name := suffix.Name
-				artist := suffix.Artist
-				fmt.Println("I chose to add", name)
-				song = lastFm.Song{Artist: artist, Title: name}
-			} else { // there's only one choice.
-				name := chain[prefix].Suffixes[0].Name
-				artist := chain[prefix].Suffixes[0].Artist
-				fmt.Println("Only one choice. I chose to add", name)
-				song = lastFm.Song{Artist: artist, Title: name}
 			}
-			return song, nil
-		} else {
-			return lastFm.Song{}, errors.New("Couldn't find the song for that prefix.")
+
+			sort.Sort(cdf) // making the CDF is much easier with sorting first.
+
+			// Creating the cdf here
+			for j := 1; j < len(cdf); j++ {
+				cdf[j][0] = cdf[j-1][0]
+			}
+			// Now to do the search
+			suffix := suffixes[searchCDF(cdf)]
+			name := suffix.Name
+			artist := suffix.Artist
+			fmt.Println("I chose to add", name)
+			song = lastFm.Song{Artist: artist, Title: name}
+		} else { // there's only one choice.
+			name := chain[prefix].Suffixes[0].Name
+			artist := chain[prefix].Suffixes[0].Artist
+			fmt.Println("Only one choice. I chose to add", name)
+			song = lastFm.Song{Artist: artist, Title: name}
 		}
+		return song, nil
+	} else {
+		return lastFm.Song{}, errors.New("Couldn't find the song for that prefix.")
+	}
 }
 
 // Sort interface implementation
@@ -284,7 +261,7 @@ func (cdf CDF) Swap(i, j int) {
 // Binary search
 func searchCDF(cdf CDF) int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	num := r.Intn(len(cdf) - 1) + 1 // Doing the -1 and +1 because Intn can return 0, which isn't valid
+	num := r.Intn(len(cdf)-1) + 1 // Doing the -1 and +1 because Intn can return 0, which isn't valid
 	// Binary search!
 	right := len(cdf) - 1
 	left := 0
@@ -297,28 +274,28 @@ func searchCDF(cdf CDF) int {
 			index = m
 			done = true
 		} else if am < num {
-			if m == 0  || m == len(cdf) - 1 {
+			if m == 0 || m == len(cdf)-1 {
 				index = m
 				done = true
 			} else if cdf[m+1][0] > num {
-				index = m+1
+				index = m + 1
 				done = true
 			} else {
-				left = m+1
+				left = m + 1
 			}
 		} else {
-			if m == 0 || m == len(cdf) - 1 {
+			if m == 0 || m == len(cdf)-1 {
 				index = m
 				done = true
 			} else if cdf[m-1][0] < num {
 				index = m
 				done = true
 			} else {
-				right = m-1
+				right = m - 1
 			}
 		}
 	}
-	if done == false || index < 0 || index > len(cdf) - 1 {
+	if done == false || index < 0 || index > len(cdf)-1 {
 		panic("Something went wrong in the binary search")
 	}
 	return cdf[index][1]
