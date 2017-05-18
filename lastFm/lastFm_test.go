09c7a964a710d03b819ae8b03717f8778177a78d
Kristian Snyder <knsnyder96@gmail.com>
2017-05-18 09:02:44 -0400

    Added tests for caching

---
+++ lastFm/lastFm_test.go
@@ -0,0 +1,49 @@
+package lastFm
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSongMapCaching(t *testing.T) {
+	// create a very basic test object
+	s := SongMap{}
+	s.Songs = make(map[BaseSong]bool)
+	s.Songs[BaseSong{"title", "artist"}] = true
+
+	// make a caching attempt
+	err := cacheUniqueSongs("test", s)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// clean up after oneself
+	os.RemoveAll("./cached-unique-songs")
+}
+
+func TestSongMapReading(t *testing.T) {
+	// create a very basic test object
+	s := SongMap{}
+	s.Songs = make(map[BaseSong]bool)
+	s.Songs[BaseSong{"title", "artist"}] = true
+
+	// make a caching attempt
+	err := cacheUniqueSongs("test", s)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	// see if we can read it back
+	s = SongMap{}
+	err = ReadCachedUniqueSongs("test", &s)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	_, ok := s.Songs[BaseSong{"title", "artist"}]
+	if !ok {
+		t.Error("The returned cache didn't contain the correct item.")
+	}
+
+	// clean up after oneself
+	os.RemoveAll("./cached-unique-songs")
+}
